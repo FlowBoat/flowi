00f769afe01dfd1b55720be1f02de1fec9b3775c
Atif Mahmud <atifmahmud101@gmail.com>
2018-03-14 13:54:56 -0400

    Design DocPipe
    References: #3

--- src/pipes/doc/doc.ts
+++ src/pipes/doc/doc.ts
@@ -1,10 +1,27 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { FirestoreProvider } from '../../providers/firestore/firestore';
+import { Observable } from 'rxjs/Observable';
 
 @Pipe({
   name: 'doc',
 })
 export class DocPipe implements PipeTransform {
-  transform(value: string, ...args) {
-    return value;
+  constructor(private db: FirestoreProvider) {}
+
+  transform(value: any): Observable<any> {
+    return this.db.doc$(value.path);
   }
+
 }
+
+/**
+ * DOCUMENTATION
+ *
+ * Usage:
+ * If we are trying to easily display a property of a referenced document
+ * we can employ this pipe.
+ *
+ * <div *ngIf="dealDoc | async as deal">
+ *     {{ (deal.business | doc | async)?.name }}
+ * </div>
+ */
