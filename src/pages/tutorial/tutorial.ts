4bc9526e45abb709bb4f1c1eea08a10943c3cc1f
Atif Mahmud <atifmahmud101@gmail.com>
2018-03-14 14:22:44 -0400

    Generate TutorialPage scaffold
    References: #5

---
+++ src/pages/tutorial/tutorial.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-tutorial',
+  templateUrl: 'tutorial.html',
+})
+export class TutorialPage {
+
+  constructor() {}
+
+}
