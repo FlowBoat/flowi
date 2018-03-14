b3df2e64d82088f0404aba74faf02ce098c35f81
Atif Mahmud <atifmahmud101@gmail.com>
2018-03-14 14:34:47 -0400

    Insert static slide variables
    References: #5

--- src/pages/tutorial/tutorial.ts
+++ src/pages/tutorial/tutorial.ts
@@ -16,7 +16,31 @@ export interface Slide {
   templateUrl: 'tutorial.html',
 })
 export class TutorialPage {
+  slides: Slide[];
+  showSkip = true;
+  dir: string = 'ltr';
 
-  constructor(public navCtrl: NavController, public menu: MenuController, public platform: Platform) {}
+  constructor(public navCtrl: NavController, public menu: MenuController, public platform: Platform) {
+    this.dir = platform.dir();
+    this.slides = [
+          {
+            title: 'Welcome to Flowi',
+            description: 'Flowi is the <b>smartest</b> advertising solution for dense city centres. Get access to relevant and amazing deals' +
+            ' without any hassle.',
+            image: 'assets/img/ica-slidebox-img-1.png',
+          },
+          {
+            title: 'How to use Flowi',
+            description: 'Choose to <b>browse</b>, <b>be notified</b>, or <b>seek</b> all kinds of deals through our web app. When you walk by a store' +
+            ' with a <b>Flowi</b> logo, be sure to check your phone for an exclusive push notification. That\'s it, simple right?',
+            image: 'assets/img/ica-slidebox-img-2.png',
+          },
+          {
+            title: 'The Best Part',
+            description: 'Flowi is totally <b>free</b>. You can even close your browser; you\'ll still receive relevant push notifications and fast, reliable Wifi.',
+            image: 'assets/img/ica-slidebox-img-3.png',
+          }
+        ]
+  }
 
 }
