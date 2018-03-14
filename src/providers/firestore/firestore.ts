f55c300298dd2dbc1fe2e02af24d20931aa8ce5e
Atif Mahmud <atifmahmud101@gmail.com>
2018-03-14 13:38:33 -0400

    Add imports
    References: #3

--- src/providers/firestore/firestore.ts
+++ src/providers/firestore/firestore.ts
@@ -1,10 +1,17 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+// RXJS
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/take";
+import "rxjs/add/operator/do";
+
+// Firebase
+import * as firebase from "firebase";
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 
 @Injectable()
 export class FirestoreProvider {
 
-  constructor(public http: HttpClient) {
-  }
+  constructor() { }
 
 }
